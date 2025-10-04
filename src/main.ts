2711bc20a309a9d59572feb137493e0c48c31f8e
Brayanmosquera345 <davidmosquera360@gmail.com>
2025-10-04 09:44:28 -0500

    feat(api): agregar versionamiento en endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { ErrorsInterceptor } from './common/interceptors/errors.interceptor';
@@ -11,9 +11,20 @@ async function bootstrap() {
     .setTitle('Task Manager')
     .setDescription('Api for managing tasks')
     .setVersion('1.0')
+    .addServer(
+      `http://localhost:${process.env.PORT ?? 3000}/api/v1`,
+      'Entorno Local',
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api-docs', app, document);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
